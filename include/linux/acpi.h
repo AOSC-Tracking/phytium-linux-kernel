d52fe9be6fcd02590a66b23b2d1597653876e70a
Robert Richter <rrichter@amd.com>
2024-05-10 17:52:42 +0100

    ACPI/NUMA: Remove architecture dependent remainings
    With the removal of the Itanium architecture [1]
    the last architecture dependent functions:
    
     acpi_numa_slit_init(), acpi_numa_memory_affinity_init()
    
    were removed. Remove its remainings in the header
    files too and make them static.
    
     [1] commit cf8e8658100d ("arch: Remove Itanium (IA-64) architecture")
    
    Mainline: Open-Source
    commit 3a785e19f432672b9ef53f07c506d5e698439033 upstream.
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Alison Schofield <alison.schofield@intel.com>
    Signed-off-by: Robert Richter <rrichter@amd.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Yuquan Wang <wangyuquan1236@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: Iee146c2c34bf6c5a74248f2d07f8f281b6d17625

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -261,9 +261,6 @@ static inline bool acpi_gicc_is_usable(struct acpi_madt_generic_interrupt *gicc)
 	return gicc->flags & (ACPI_MADT_ENABLED | ACPI_MADT_GICC_CPU_CAPABLE);
 }
 
-/* the following numa functions are architecture-dependent */
-void acpi_numa_slit_init (struct acpi_table_slit *slit);
-
 #if defined(CONFIG_X86) || defined(CONFIG_IA64) || defined(CONFIG_LOONGARCH)
 void acpi_numa_processor_affinity_init (struct acpi_srat_cpu_affinity *pa);
 #else
@@ -285,8 +282,6 @@ void acpi_numa_gicc_affinity_init(struct acpi_srat_gicc_affinity *pa);
 static inline void
 acpi_numa_gicc_affinity_init(struct acpi_srat_gicc_affinity *pa) { }
 #endif
-
-int acpi_numa_memory_affinity_init (struct acpi_srat_mem_affinity *ma);
 
 #ifndef PHYS_CPUID_INVALID
 typedef u32 phys_cpuid_t;
