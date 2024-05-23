0d809484061fb293bb73c9f1971a2e92f651d398
Liu Tao <liutao@phytium.com.cn>
2024-05-23 15:07:33 +0800

    drm/ttm: Fix qemu bad address error with phytium vgpu acceleration enabled
    With phytium virtual gpu acceleration method in qemu windows guest OS,
    guest OS driver needs to share GPU device memory with host and requires
    the shared buffer mapping remains valid, but the ttm map implemenataion
    did not account for this usecase. So we add a ref count for every pages
    of the shared buffer.
    
    Signed-off-by: Liu Tao <liutao@phytium.com.cn>
    Signed-off-by: Li Mingzhe <limingzhe1839@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I20cf405757e6aa7fd6953fb603dc8c455badefde

--- drivers/gpu/drm/ttm/ttm_tt.c
+++ drivers/gpu/drm/ttm/ttm_tt.c
@@ -450,8 +450,10 @@ static void ttm_tt_add_mapping(struct ttm_tt *ttm)
 	if (ttm->page_flags & TTM_PAGE_FLAG_SG)
 		return;
 
-	for (i = 0; i < ttm->num_pages; ++i)
+	for (i = 0; i < ttm->num_pages; ++i) {
 		ttm->pages[i]->mapping = ttm->bdev->dev_mapping;
+		page_ref_inc(ttm->pages[i]);
+	}
 }
 
 int ttm_tt_populate(struct ttm_tt *ttm, struct ttm_operation_ctx *ctx)
@@ -481,6 +483,7 @@ static void ttm_tt_clear_mapping(struct ttm_tt *ttm)
 	for (i = 0; i < ttm->num_pages; ++i) {
 		(*page)->mapping = NULL;
 		(*page++)->index = 0;
+		page_ref_dec(ttm->pages[i]);
 	}
 }
 
