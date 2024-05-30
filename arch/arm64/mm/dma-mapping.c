1afc793312733825cf5582e57f38636adc8a0438
Cui Chao <cuichao1753@phytium.com.cn>
2024-05-30 08:54:27 +0800

    pswiotlb: Distribute the traffic to PSWIOTLB or not
    The traffic is distributed to Phytium software IO tlb
    when the platform is PS23064 SoC and the type of device
    is pci/pcie.
    
    Mainline: NA
    Signed-off-by: Cui Chao <cuichao1753@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I228dd83377e7aa6e628568a1e3bc8bb8b76c4c17

--- arch/arm64/mm/dma-mapping.c
+++ arch/arm64/mm/dma-mapping.c
@@ -29,10 +29,23 @@
 #include <linux/vmalloc.h>
 #include <linux/swiotlb.h>
 #include <linux/pci.h>
+#include <linux/pswiotlb.h>
 
 #include <asm/cacheflush.h>
 
 static int swiotlb __ro_after_init;
+
+void arch_sync_dma_for_device(phys_addr_t paddr, size_t size,
+		enum dma_data_direction dir)
+{
+	__dma_map_area(phys_to_virt(paddr), size, dir);
+}
+
+void arch_sync_dma_for_cpu(phys_addr_t paddr, size_t size,
+		enum dma_data_direction dir)
+{
+	__dma_unmap_area(phys_to_virt(paddr), size, dir);
+}
 
 static pgprot_t __get_dma_pgprot(unsigned long attrs, pgprot_t prot,
 				 bool coherent)
@@ -161,6 +174,13 @@ static dma_addr_t __swiotlb_map_page(struct device *dev, struct page *page,
 {
 	dma_addr_t dev_addr;
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		dev_addr = pswiotlb_dma_direct_map_page_distribute(dev,
+					page, offset, size, dir, attrs);
+		return dev_addr;
+	}
+#endif
 	dev_addr = swiotlb_map_page(dev, page, offset, size, dir, attrs);
 	if (!is_device_dma_coherent(dev) &&
 	    (attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
@@ -177,6 +197,13 @@ static void __swiotlb_unmap_page(struct device *dev, dma_addr_t dev_addr,
 	if (!is_device_dma_coherent(dev) &&
 	    (attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
 		__dma_unmap_area(phys_to_virt(dma_to_phys(dev, dev_addr)), size, dir);
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_direct_unmap_page_attrs_distribute(dev, dev_addr, size, dir, attrs);
+		return;
+	}
+#endif
+
 	swiotlb_unmap_page(dev, dev_addr, size, dir, attrs);
 }
 
@@ -187,6 +214,12 @@ static int __swiotlb_map_sg_attrs(struct device *dev, struct scatterlist *sgl,
 	struct scatterlist *sg;
 	int i, ret;
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		ret = pswiotlb_dma_direct_map_sg_attrs_distribute(dev, sgl, nelems, dir, attrs);
+		return ret;
+	}
+#endif
 	ret = swiotlb_map_sg_attrs(dev, sgl, nelems, dir, attrs);
 	if (!is_device_dma_coherent(dev) &&
 	    (attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
@@ -210,6 +243,12 @@ static void __swiotlb_unmap_sg_attrs(struct device *dev,
 		for_each_sg(sgl, sg, nelems, i)
 			__dma_unmap_area(phys_to_virt(dma_to_phys(dev, sg->dma_address)),
 					 sg->length, dir);
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_direct_unmap_sg_attrs_distribute(dev, sgl, nelems, dir, attrs);
+		return;
+	}
+#endif
 	swiotlb_unmap_sg_attrs(dev, sgl, nelems, dir, attrs);
 }
 
@@ -219,6 +258,13 @@ static void __swiotlb_sync_single_for_cpu(struct device *dev,
 {
 	if (!is_device_dma_coherent(dev))
 		__dma_unmap_area(phys_to_virt(dma_to_phys(dev, dev_addr)), size, dir);
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_direct_sync_single_for_cpu_distribute(dev, dev_addr, size, dir);
+		return;
+	}
+#endif
+
 	swiotlb_sync_single_for_cpu(dev, dev_addr, size, dir);
 }
 
@@ -226,6 +272,12 @@ static void __swiotlb_sync_single_for_device(struct device *dev,
 					     dma_addr_t dev_addr, size_t size,
 					     enum dma_data_direction dir)
 {
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_direct_sync_single_for_device_distribute(dev, dev_addr, size, dir);
+		return;
+	}
+#endif
 	swiotlb_sync_single_for_device(dev, dev_addr, size, dir);
 	if (!is_device_dma_coherent(dev))
 		__dma_map_area(phys_to_virt(dma_to_phys(dev, dev_addr)), size, dir);
@@ -242,6 +294,12 @@ static void __swiotlb_sync_sg_for_cpu(struct device *dev,
 		for_each_sg(sgl, sg, nelems, i)
 			__dma_unmap_area(phys_to_virt(dma_to_phys(dev, sg->dma_address)),
 					 sg->length, dir);
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_direct_sync_sg_for_cpu_distribute(dev, sgl, nelems, dir);
+		return;
+	}
+#endif
 	swiotlb_sync_sg_for_cpu(dev, sgl, nelems, dir);
 }
 
@@ -252,6 +310,12 @@ static void __swiotlb_sync_sg_for_device(struct device *dev,
 	struct scatterlist *sg;
 	int i;
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_direct_sync_sg_for_device_distribute(dev, sgl, nelems, dir);
+		return;
+	}
+#endif
 	swiotlb_sync_sg_for_device(dev, sgl, nelems, dir);
 	if (!is_device_dma_coherent(dev))
 		for_each_sg(sgl, sg, nelems, i)
@@ -719,6 +783,17 @@ static void __iommu_sync_single_for_cpu(struct device *dev,
 {
 	phys_addr_t phys;
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		if (!is_device_dma_coherent(dev)) {
+			phys = iommu_iova_to_phys(iommu_get_domain_for_dev(dev), dev_addr);
+			__dma_unmap_area(phys_to_virt(phys), size, dir);
+		}
+		pswiotlb_dma_iommu_sync_single_for_cpu_distribute(dev, dev_addr, size, dir);
+
+		return;
+	}
+#endif
 	if (is_device_dma_coherent(dev))
 		return;
 
@@ -732,6 +807,17 @@ static void __iommu_sync_single_for_device(struct device *dev,
 {
 	phys_addr_t phys;
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_iommu_sync_single_for_device_distribute(dev, dev_addr, size, dir);
+		if (!is_device_dma_coherent(dev)) {
+			phys = iommu_iova_to_phys(iommu_get_domain_for_dev(dev), dev_addr);
+			__dma_map_area(phys_to_virt(phys), size, dir);
+		}
+
+		return;
+	}
+#endif
 	if (is_device_dma_coherent(dev))
 		return;
 
@@ -746,8 +832,19 @@ static dma_addr_t __iommu_map_page(struct device *dev, struct page *page,
 {
 	bool coherent = is_device_dma_coherent(dev);
 	int prot = dma_info_to_prot(dir, coherent, attrs);
-	dma_addr_t dev_addr = iommu_dma_map_page(dev, page, offset, size, prot);
+#ifdef CONFIG_PSWIOTLB
+	dma_addr_t dev_addr;
 
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		dev_addr =
+			pswiotlb_dma_iommu_map_page_distribute(dev, page, offset, size, dir, attrs);
+
+		return dev_addr;
+	}
+	dev_addr = iommu_dma_map_page(dev, page, offset, size, prot);
+#else
+	dma_addr_t dev_addr = iommu_dma_map_page(dev, page, offset, size, prot);
+#endif
 	if (!iommu_dma_mapping_error(dev, dev_addr) &&
 	    (attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
 		__iommu_sync_single_for_device(dev, dev_addr, size, dir);
@@ -759,6 +856,13 @@ static void __iommu_unmap_page(struct device *dev, dma_addr_t dev_addr,
 			       size_t size, enum dma_data_direction dir,
 			       unsigned long attrs)
 {
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_iommu_unmap_page_attrs_distribute(dev, dev_addr, size, dir, attrs);
+		return;
+	}
+#endif
+
 	if ((attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
 		__iommu_sync_single_for_cpu(dev, dev_addr, size, dir);
 
@@ -771,6 +875,13 @@ static void __iommu_sync_sg_for_cpu(struct device *dev,
 {
 	struct scatterlist *sg;
 	int i;
+
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_iommu_sync_sg_for_cpu_distribute(dev, sgl, nelems, dir);
+		return;
+	}
+#endif
 
 	if (is_device_dma_coherent(dev))
 		return;
@@ -786,6 +897,12 @@ static void __iommu_sync_sg_for_device(struct device *dev,
 	struct scatterlist *sg;
 	int i;
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_iommu_sync_sg_for_device_distribute(dev, sgl, nelems, dir);
+		return;
+	}
+#endif
 	if (is_device_dma_coherent(dev))
 		return;
 
@@ -802,6 +919,13 @@ static int __iommu_map_sg_attrs(struct device *dev, struct scatterlist *sgl,
 	if ((attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
 		__iommu_sync_sg_for_device(dev, sgl, nelems, dir);
 
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		return pswiotlb_dma_iommu_map_sg_attrs_distribute(dev, sgl, nelems,
+					dma_info_to_prot(dir, coherent, attrs), attrs);
+	}
+#endif
+
 	return iommu_dma_map_sg(dev, sgl, nelems,
 				dma_info_to_prot(dir, coherent, attrs));
 }
@@ -813,6 +937,13 @@ static void __iommu_unmap_sg_attrs(struct device *dev,
 {
 	if ((attrs & DMA_ATTR_SKIP_CPU_SYNC) == 0)
 		__iommu_sync_sg_for_cpu(dev, sgl, nelems, dir);
+
+#ifdef CONFIG_PSWIOTLB
+	if (check_if_pswiotlb_is_applicable(dev)) {
+		pswiotlb_dma_iommu_unmap_sg_attrs_distribute(dev, sgl, nelems, dir, attrs);
+		return;
+	}
+#endif
 
 	iommu_dma_unmap_sg(dev, sgl, nelems, dir, attrs);
 }
