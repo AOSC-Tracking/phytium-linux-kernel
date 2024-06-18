ddbcf77a3ec1c53f9ef830f2f07b26a3531f975e
Tejun Heo <tj@kernel.org>
2024-06-18 10:09:20 -1000

    sched_ext: Implement sched_ext_ops.cpu_online/offline()
    Add ops.cpu_online/offline() which are invoked when CPUs come online and
    offline respectively. As the enqueue path already automatically bypasses
    tasks to the local dsq on a deactivated CPU, BPF schedulers are guaranteed
    to see tasks only on CPUs which are between online() and offline().
    
    If the BPF scheduler doesn't implement ops.cpu_online/offline(), the
    scheduler is automatically exited with SCX_ECODE_RESTART |
    SCX_ECODE_RSN_HOTPLUG. Userspace can implement CPU hotpplug support
    trivially by simply reinitializing and reloading the scheduler.
    
    scx_qmap is updated to print out online CPUs on hotplug events. Other
    schedulers are updated to restart based on ecode.
    
    v3: - The previous implementation added @reason to
          sched_class.rq_on/offline() to distinguish between CPU hotplug events
          and topology updates. This was buggy and fragile as the methods are
          skipped if the current state equals the target state. Instead, add
          scx_rq_[de]activate() which are directly called from
          sched_cpu_de/activate(). This also allows ops.cpu_on/offline() to
          sleep which can be useful.
    
        - ops.dispatch() could be called on a CPU that the BPF scheduler was
          told to be offline. The dispatch patch is updated to bypass in such
          cases.
    
    v2: - To accommodate lock ordering change between scx_cgroup_rwsem and
          cpus_read_lock(), CPU hotplug operations are put into its own SCX_OPI
          block and enabled eariler during scx_ope_enable() so that
          cpus_read_lock() can be dropped before acquiring scx_cgroup_rwsem.
    
        - Auto exit with ECODE added.
    
    [ Upstream commit 60c27fb59f6cffa73fc8c60e3a22323c78044576 ]
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Acked-by: Josh Don <joshdon@google.com>
    Acked-by: Hao Luo <haoluo@google.com>
    Acked-by: Barret Rhoden <brho@google.com>
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4880
    Backported-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: Ibf2d61835c9b6b53debc8b1528c19faf6578b649

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8041,6 +8041,8 @@ int sched_cpu_activate(unsigned int cpu)
 		cpuset_cpu_active();
 	}
 
+	scx_rq_activate(rq);
+
 	/*
 	 * Put the rq online, if not already. This happens:
 	 *
@@ -8075,6 +8077,8 @@ int sched_cpu_deactivate(unsigned int cpu)
 	 * sched_cpu_dying().
 	 */
 	balance_push_set(cpu, true);
+
+	scx_rq_deactivate(rq);
 
 	/*
 	 * We've cleared cpu_active_mask / set balance_push, wait for all
