f30b7a26b0873d077b564f8e50803f4c7ca2e633
Dai Jingtao <daijingtao1503@phytium.com.cn>
2024-07-23 17:17:37 +0800

    i2s: phytium: Add I2S driver version information
    This patch add driver version information, which will be
    used to synchronize and manage driver patches in the future.
    
    Mainline: Open-Source
    Signed-off-by: Dai Jingtao <daijingtao1503@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I0b35df60f5b518d0439bab009e6d204413059825

--- sound/soc/codecs/es8336.c
+++ sound/soc/codecs/es8336.c
@@ -30,6 +30,8 @@
 #include <linux/irq.h>
 #include <linux/regmap.h>
 #include "es8336.h"
+
+#define ES8336_V1_VERSION "1.0.0"
 #define ES8336_MUTE (1 << 5)
 
 static struct snd_soc_component *es8336_component;
@@ -1071,3 +1073,4 @@ static struct i2c_driver es8336_i2c_driver = {
 module_i2c_driver(es8336_i2c_driver);
 MODULE_DESCRIPTION("ASoC es8336 driver");
 MODULE_LICENSE("GPL");
+MODULE_VERSION(ES8336_V1_VERSION);
