fca2e05e2546802e65f0909a680102d5a3b0ecda
Johannes Weiner <hannes@cmpxchg.org>
2024-10-11 10:49:33 +0200

    sched/psi: Fix mistaken CPU pressure indication after corrupted task state bug
    Since sched_delayed tasks remain queued even after blocking, the load
    balancer can migrate them between runqueues while PSI considers them
    to be asleep. As a result, it misreads the migration requeue followed
    by a wakeup as a double queue:
    
      psi: inconsistent task state! task=... cpu=... psi_flags=4 clear=. set=4
    
    First, call psi_enqueue() after p->sched_class->enqueue_task(). A
    wakeup will clear p->se.sched_delayed while a migration will not, so
    psi can use that flag to tell them apart.
    
    Then teach psi to migrate any "sleep" state when delayed-dequeue tasks
    are being migrated.
    
    Delayed-dequeue tasks can be revived by ttwu_runnable(), which will
    call down with a new ENQUEUE_DELAYED. Instead of further complicating
    the wakeup conditional in enqueue_task(), identify migration contexts
    instead and default to wakeup handling for all other cases.
    
    It's not just the warning in dmesg, the task state corruption causes a
    permanent CPU pressure indication, which messes with workload/machine
    health monitoring.
    
    [ Upstream commit c6508124193d42bbc3224571eb75bfa4c1821fbb ]
    Debugged-by-and-original-fix-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Fixes: 152e11f6df29 ("sched/fair: Implement delayed dequeue")
    Closes: https://lore.kernel.org/lkml/20240830123458.3557-1-spasswolf@web.de/
    Closes: https://lore.kernel.org/all/cd67fbcd-d659-4822-bb90-7e8fbb40a856@molgen.mpg.de/
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://lkml.kernel.org/r/20241010193712.GC181795@cmpxchg.org
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4613
    Backported-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I5dedd6a7f8ec361b3ac3ba3016225ade9996520e

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2099,13 +2099,13 @@ static inline void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	if (!(flags & ENQUEUE_NOCLOCK))
 		update_rq_clock(rq);
 
-	if (!(flags & ENQUEUE_RESTORE)) {
-		sched_info_enqueue(rq, p);
-		psi_enqueue(p, (flags & ENQUEUE_WAKEUP) && !(flags & ENQUEUE_MIGRATED));
-	}
-
 	uclamp_rq_inc(rq, p);
 	p->sched_class->enqueue_task(rq, p, flags);
+
+	if (!(flags & ENQUEUE_RESTORE)) {
+		sched_info_enqueue(rq, p);
+		psi_enqueue(p, flags & ENQUEUE_MIGRATED);
+	}
 
 	if (sched_core_enabled(rq))
 		sched_core_enqueue(rq, p);
@@ -2124,7 +2124,7 @@ inline bool dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!(flags & DEQUEUE_SAVE)) {
 		sched_info_dequeue(rq, p);
-		psi_dequeue(p, flags & DEQUEUE_SLEEP);
+		psi_dequeue(p, !(flags & DEQUEUE_SLEEP));
 	}
 
 	uclamp_rq_dec(rq, p);
