2851e7f3234f63b929bf8e99e645b94a2ddf6efb
liutianyu1250 <liutianyu1250@phytium.com.cn>
2024-12-09 16:37:16 +0800

    Merge tag 'v6.6.63' into v6.6-phytium-embedded
    This is the 6.6.63 stable release
    
    Fix Conflicts:
    	arch/arm64/include/asm/cputype.h
    	drivers/mfd/Makefile
    	drivers/net/wireless/realtek/rtw88/rtw8821cu.c
    	drivers/usb/host/xhci.h
    	kernel/power/swap.c

--- drivers/net/wireless/intel/iwlwifi/pcie/rx.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/rx.c
@@ -1385,7 +1385,7 @@ static void iwl_pcie_rx_handle_rb(struct iwl_trans *trans,
 		 * if it is true then one of the handlers took the page.
 		 */
 
-		if (reclaim) {
+		if (reclaim && txq) {
 			u16 sequence = le16_to_cpu(pkt->hdr.sequence);
 			int index = SEQ_TO_INDEX(sequence);
 			int cmd_index = iwl_txq_get_cmd_index(txq, index);
@@ -1510,7 +1510,7 @@ restart:
 	spin_lock(&rxq->lock);
 	/* uCode's read index (stored in shared DRAM) indicates the last Rx
 	 * buffer that the driver may process (last buffer filled by ucode). */
-	r = le16_to_cpu(iwl_get_closed_rb_stts(trans, rxq)) & 0x0FFF;
+	r = iwl_get_closed_rb_stts(trans, rxq);
 	i = rxq->read;
 
 	/* W/A 9000 device step A0 wrap-around bug */
