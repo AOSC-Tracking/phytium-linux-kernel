4c8ac3e0715d78b93baa035cac99502f8a0c284b
Honglei Wang <jameshongleiwang@126.com>
2025-01-08 10:33:28 +0800

    sched_ext: switch class when preempted by higher priority scheduler
    ops.cpu_release() function, if defined, must be invoked when preempted by
    a higher priority scheduler class task. This scenario was skipped in
    commit f422316d7466 ("sched_ext: Remove switch_class_scx()"). Let's fix
    it.
    
    [ Upstream commit 68e449d849fd50bd5e61d8bd32b3458dbd3a3df6 ]
    Fixes: f422316d7466 ("sched_ext: Remove switch_class_scx()")
    Signed-off-by: Honglei Wang <jameshongleiwang@126.com>
    Acked-by: Andrea Righi <arighi@nvidia.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Backported-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: Ib418407005b5b53414cb71f76ec83a1c2b162a86

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -3044,7 +3044,7 @@ static void put_prev_task_scx(struct rq *rq, struct task_struct *p,
 		 */
 		if (p->scx.slice && !scx_rq_bypassing(rq)) {
 			dispatch_enqueue(&rq->scx.local_dsq, p, SCX_ENQ_HEAD);
-			return;
+			goto switch_class;
 		}
 
 		/*
@@ -3061,6 +3061,7 @@ static void put_prev_task_scx(struct rq *rq, struct task_struct *p,
 		}
 	}
 
+switch_class:
 	if (next && next->sched_class != &ext_sched_class)
 		switch_class(rq, next);
 }
