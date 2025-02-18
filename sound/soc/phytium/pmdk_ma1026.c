0355d3cb7d7dbee809eeb50ab7d26c5992b9cc8f
shiguangyuan1029 <shiguangyuan1029@phytium.com.cn>
2025-02-18 22:20:37 +0800

    dirver: i2s: implement ma1026 support for ACPI

--- sound/soc/phytium/pmdk_ma1026.c
+++ sound/soc/phytium/pmdk_ma1026.c
@@ -9,7 +9,7 @@
 #include <sound/pcm_params.h>
 #include <sound/jack.h>
 
-#define PMDK_ma1026_VERSION "1.0.0"
+#define PMDK_MA1026_VERSION "1.0.0"
 
 /* PMDK widgets */
 static const struct snd_soc_dapm_widget pmdk_ma1026_dapm_widgets[] = {
@@ -40,12 +40,12 @@ static const struct snd_soc_dapm_route pmdk_ma1026_audio_map[] = {
 
 static struct snd_soc_dai_link pmdk_dai[] = {
 	{
-		.name = "ma1026 HIFI",
-		.stream_name = "ma1026 HIFI",
+		.name = "MA1026 ASP",
+		.stream_name = "MA1026 ASP",
 		.cpu_dai_name = "phytium-i2s-lsd",
-		.codec_dai_name = "ma1026-hifi",
+		.codec_dai_name = "ma1026-asp",
 		.platform_name = "snd-soc-dummy",
-		.codec_name = "i2c-ma1026:00",
+		.codec_name = "i2c-MAMA1026:00",
 		.dai_fmt = PMDK_DAI_FMT,
 	},
 };
@@ -55,19 +55,13 @@ static struct snd_soc_card pmdk = {
 	.owner = THIS_MODULE,
 	.dai_link = pmdk_dai,
 	.num_links = ARRAY_SIZE(pmdk_dai),
-
-	.dapm_widgets = pmdk_ma1026_dapm_widgets,
-	.num_dapm_widgets = ARRAY_SIZE(pmdk_ma1026_dapm_widgets),
-	.controls = pmdk_ma1026_controls,
-	.num_controls = ARRAY_SIZE(pmdk_ma1026_controls),
-	.dapm_routes = pmdk_ma1026_audio_map,
-	.num_dapm_routes = ARRAY_SIZE(pmdk_ma1026_audio_map),
 };
 
 static int pmdk_sound_probe(struct platform_device *pdev)
 {
 	struct snd_soc_card *card = &pmdk;
 	struct device *dev = &pdev->dev;
+
 	card->dev = dev;
 	return devm_snd_soc_register_card(&pdev->dev, card);
 }
@@ -79,7 +73,7 @@ static const struct acpi_device_id pmdk_sound_acpi_match[] = {
 MODULE_DEVICE_TABLE(acpi, pmdk_sound_acpi_match);
 
 static const struct of_device_id phytium_ma1026_of_match[] = {
-	{ .compatible = "phytium,audio-ma1026", },
+	{ .compatible = "phytium,audio-ma1026",},
 	{},
 };
 
@@ -97,7 +91,7 @@ static struct platform_driver pmdk_sound_driver = {
 
 module_platform_driver(pmdk_sound_driver);
 
-MODULE_AUTHOR("harry guo <harry@cubiclattice.com>");
-MODULE_DESCRIPTION("ALSA SoC PMDK ma1026");
+MODULE_AUTHOR("Shi Guangyuan <shiguangyuan1029@phytium.com.cn>");
+MODULE_DESCRIPTION("ALSA SoC PMDK MA1026");
 MODULE_LICENSE("GPL");
-MODULE_VERSION(PMDK_ma1026_VERSION);
+MODULE_VERSION(PMDK_MA1026_VERSION);
