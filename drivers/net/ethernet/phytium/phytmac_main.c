0f3258fad6d2cab620e609cffae09ec65e85595c
Li Wencheng <liwencheng@phytium.com.cn>
2025-03-28 15:50:09 +0800

    net/phytmac: Clear RX descriptor address after the skb construction
    If the skb build failed, the descriptor address has been cleared,
    and the current descriptor will be considered invalid in the next
    round of processing.
    
    Mainline: Open-Source
    Signed-off-by: Li Wencheng <liwencheng@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I91261f1db9b762aee3996166cb22f2960a82be1f

--- drivers/net/ethernet/phytium/phytmac_main.c
+++ drivers/net/ethernet/phytium/phytmac_main.c
@@ -955,8 +955,6 @@ static struct sk_buff *phytmac_rx_xdp_single(struct phytmac_queue *queue,
 	len = hw_if->get_rx_pkt_len(pdata, desc);
 	rx_buffer = phytmac_get_rx_buffer(queue, queue->rx_tail, len);
 
-	hw_if->zero_rx_desc_addr(desc);
-
 	xdp.data = page_address(rx_buffer->page) + rx_buffer->page_offset;
 	xdp.data_meta = xdp.data;
 	xdp.data_hard_start = xdp.data - PHYTMAC_SKB_PAD;
@@ -978,6 +976,7 @@ static struct sk_buff *phytmac_rx_xdp_single(struct phytmac_queue *queue,
 		} else {
 			rx_buffer->pagecnt_bias++;
 		}
+		hw_if->zero_rx_desc_addr(desc);
 		phytmac_put_rx_buffer(queue, rx_buffer);
 		pdata->ndev->stats.rx_bytes += len;
 		queue->stats.rx_bytes += len;
@@ -998,7 +997,6 @@ static struct sk_buff *phytmac_rx_single(struct phytmac_queue *queue, struct phy
 
 	len = hw_if->get_rx_pkt_len(pdata, desc);
 	rx_buffer = phytmac_get_rx_buffer(queue, queue->rx_tail, len);
-	hw_if->zero_rx_desc_addr(desc);
 
 	skb = phytmac_build_skb(rx_buffer, len);
 	if (unlikely(!skb)) {
@@ -1010,6 +1008,7 @@ static struct sk_buff *phytmac_rx_single(struct phytmac_queue *queue, struct phy
 		return NULL;
 	}
 
+	hw_if->zero_rx_desc_addr(desc);
 	phytmac_put_rx_buffer(queue, rx_buffer);
 
 	skb->protocol = eth_type_trans(skb, pdata->ndev);
@@ -1045,7 +1044,6 @@ static struct sk_buff *phytmac_rx_frame(struct phytmac_queue *queue,
 
 	desc = phytmac_get_rx_desc(queue, first_frag);
 	rx_buffer = phytmac_get_rx_buffer(queue, first_frag, frag_len);
-	hw_if->zero_rx_desc_addr(desc);
 
 	skb = phytmac_build_skb(rx_buffer, frag_len);
 	if (unlikely(!skb)) {
@@ -1056,6 +1054,7 @@ static struct sk_buff *phytmac_rx_frame(struct phytmac_queue *queue,
 		return NULL;
 	}
 
+	hw_if->zero_rx_desc_addr(desc);
 	phytmac_put_rx_buffer(queue, rx_buffer);
 
 	for (frag = first_frag + 1; ; frag++) {
