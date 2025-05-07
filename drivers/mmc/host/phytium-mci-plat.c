fa8de7246a1e820c1c237b644d307d9376671393
Lai Xueyu <laixueyu1280@phytium.com.cn>
2025-05-07 17:22:13 +0800

    mmc: phytium: Add driver version information for mmc
    This patch add driver version information, which will be
    used to synchronize and manage driver patches in the future.
    
    Signed-off-by: Lai Xueyu <laixueyu1280@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I975522cb4fa2a6f4b94610dd41892757acc9019c

--- drivers/mmc/host/phytium-mci-plat.c
+++ drivers/mmc/host/phytium-mci-plat.c
@@ -193,3 +193,4 @@ module_platform_driver(phytium_mci_driver);
 MODULE_DESCRIPTION("Phytium Multimedia Card Interface driver");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Cheng Quan <chengquan@phytium.com.cn>");
+MODULE_VERSION(PHYTIUM_MMC_DRIVER_VERSION);
--- drivers/mmc/host/phytium-mci.c
+++ drivers/mmc/host/phytium-mci.c
@@ -1623,3 +1623,4 @@ EXPORT_SYMBOL(phytium_mci_common_probe);
 MODULE_DESCRIPTION("Phytium Multimedia Card Interface driver");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Cheng Quan <chengquan@phytium.com.cn>");
+MODULE_VERSION(PHYTIUM_MMC_DRIVER_VERSION);
