b4a993a45c1af76ceb636dc2fd95e1658c73389e
Xiao Cong <xiaocong1866@cpu.ac>
2025-06-11 16:17:40 +0800

    PCI: Add ACS quirk for phytium root and switch ports
    The phytium PCIe root ports and X100 switch do not support ACS at this point. However, the hardware provides isolation and source validation
    through the SMMU. The stream ID generated by the PCIe ports contain
    both the bus/device/function number as well as the port ID in its 3
    most significant bits. Turn on ACS but disable all the peer-to-peer
    features.
    
    Mainline: Open-Source
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Signed-off-by: Xiao Cong <xiaocong1866@phytium.com.cn>
    Change-Id: I468f41916ea98f7bc27744e5f9913aec47042638

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -5159,6 +5159,10 @@ static const struct pci_dev_acs_enabled {
 	{ PCI_VENDOR_ID_ZHAOXIN, PCI_ANY_ID, pci_quirk_zhaoxin_pcie_ports_acs },
 	/* Wangxun nics */
 	{ PCI_VENDOR_ID_WANGXUN, PCI_ANY_ID, pci_quirk_wangxun_nic_acs },
+	/* Phytium Technology */
+	{ 0x10b5, PCI_ANY_ID, pci_quirk_xgene_acs },
+	{ 0x17cd, PCI_ANY_ID, pci_quirk_xgene_acs },
+	{ 0x1db7, PCI_ANY_ID, pci_quirk_xgene_acs },
 	{ 0 }
 };
 
