c08bc94bba7364926d7078469816f5894b90f040
Zengruan Ye <yezengruan@huawei.com>
2025-07-23 15:45:39 +0800

    KVM/arm64: Support pvsched preempted via shared structure
    Implement the service call for configuring a shared structure between a
    vCPU and the hypervisor in which the hypervisor can tell the vCPU that is
    running or not.
    
    Mainline: NA
    (cherry picked form openEuler/OLK-6.6 commit c0d910ffc72eed3bcda04d26669f04915d956ae7)
    Signed-off-by: Zengruan Ye <yezengruan@huawei.com>
    Signed-off-by: Li ShuSen <lishusen2@huawei.com>
    Backported-by: Wu Chen <wuchen2062@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: If1808d7095b776c88062d7010d6c8bbb69803aec

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -386,6 +386,8 @@ int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 
 	kvm_arm_pvtime_vcpu_init(&vcpu->arch);
 
+	kvm_arm_pvsched_vcpu_init(&vcpu->arch);
+
 	vcpu->arch.hw_mmu = &vcpu->kvm->arch.mmu;
 
 	err = kvm_vgic_vcpu_init(vcpu);
@@ -465,6 +467,9 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 
 	if (!cpumask_test_cpu(cpu, vcpu->kvm->arch.supported_cpus))
 		vcpu_set_on_unsupported_cpu(vcpu);
+
+	if (kvm_arm_is_pvsched_enabled(&vcpu->arch))
+		kvm_update_pvsched_preempted(vcpu, 0);
 }
 
 void kvm_arch_vcpu_put(struct kvm_vcpu *vcpu)
@@ -480,6 +485,9 @@ void kvm_arch_vcpu_put(struct kvm_vcpu *vcpu)
 
 	vcpu_clear_on_unsupported_cpu(vcpu);
 	vcpu->cpu = -1;
+
+	if (kvm_arm_is_pvsched_enabled(&vcpu->arch))
+		kvm_update_pvsched_preempted(vcpu, 1);
 }
 
 static void __kvm_arm_vcpu_power_off(struct kvm_vcpu *vcpu)
