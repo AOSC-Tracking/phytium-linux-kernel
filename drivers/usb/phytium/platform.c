a670417fa49e6a00175b458cbdd14fe99a8179cb
Chen Zhenhua <chenzhenhua@phytium.com.cn>
2025-09-18 15:45:56 +0800

    usb: phytium: Add a forced operation to send the port reset command
    In the HubPortReset function, add a forced operation to send port
    reset command to fix the issue where the port reset command is not
    sent, casusing abnormal device operation.
    
    Mainline: Open-Source
    Signed-off-by: Chen Zhenhua <chenzhenhua@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I99e19220a5c4c7ab40c5b74eca53e6c5e274c490

--- drivers/usb/phytium/platform.c
+++ drivers/usb/phytium/platform.c
@@ -16,7 +16,7 @@
 #include "core.h"
 #include "hw-regs.h"
 
-#define PHYTIUM_OTG_V1_VERSION "1.0.2"
+#define PHYTIUM_OTG_V1_VERSION "1.0.3"
 #define PHYTIUM_OTG_USB_LOADED  3
 #define USB2_2_BASE_ADDRESS 0x31800000
 
