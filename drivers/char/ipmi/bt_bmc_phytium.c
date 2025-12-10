e7bd76b8c720d3e2818ef0a2e389f955556567f1
zhuling <zhuling2709@phytium.com.cn>
2025-12-10 15:51:38 +0800

    bt: phytium: Disable bt channel and mask IRQ before request interrupt
    Disable BT and mask IRQ before request interrupt. If wdt does
    not reset BT controller, the BT and IRQ enable will remain
    on. This patch ensures that the BT and IRQ is disabled before
    request interrupt to avoid BT controller'irq can't be handled.
    
    Mainline: Open-Source
    Signed-off-by: Li Yuze <liyuze@phytium.com.cn>
    Signed-off-by: zhuling <zhuling2709@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: Ic1ad4a8f4d1dfeb04d55b74e3768ad8458b79721

--- drivers/char/ipmi/bt_bmc_phytium.c
+++ drivers/char/ipmi/bt_bmc_phytium.c
@@ -72,6 +72,8 @@
 
 #define BT_BMC_BUFFER_SIZE 256
 
+#define BT_BMC_DRIVER_VERSION "1.0.0"
+
 struct bt_bmc {
 	struct device		dev;
 	struct miscdevice	miscdev;
@@ -425,6 +427,10 @@ static int bt_bmc_config_irq(struct bt_bmc *bt_bmc,
 	if (!bt_bmc->irq)
 		return -ENODEV;
 
+	rc = regmap_update_bits(bt_bmc->map,  BT_CSR1,
+				BT_CSR1_IRQ_H2B | BT_CSR1_IRQ_HWRST,
+				0);
+
 	rc = devm_request_irq(dev, bt_bmc->irq, bt_bmc_irq, IRQF_SHARED,
 			      DEVICE_NAME, bt_bmc);
 	if (rc < 0) {
@@ -479,6 +485,10 @@ static int bt_bmc_probe(struct platform_device *pdev)
 		return rc;
 	}
 
+	regmap_update_bits(bt_bmc->map,  LPC_HICR0, LPC_HICR0_LPC3E, 0);
+	regmap_update_bits(bt_bmc->map,  LPC_HICR4, LPC_BTENABLE, 0);
+	regmap_update_bits(bt_bmc->map,  LPC_HICR2, LPC_HICR2_IBFIF3, 0);
+
 	bt_bmc_config_irq(bt_bmc, pdev);
 
 	if (bt_bmc->irq) {
@@ -531,3 +541,4 @@ module_platform_driver(bt_bmc_driver);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Cheng Quan <chengquan@phytium.com.cn");
 MODULE_DESCRIPTION("Linux device interface to the BT interface");
+MODULE_VERSION(BT_BMC_DRIVER_VERSION);
