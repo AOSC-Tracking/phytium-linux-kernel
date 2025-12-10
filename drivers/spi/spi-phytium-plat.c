d7d31ff08de44dc3bb364b9a2b90c92675464b50
Peng Min <pengmin1540@phytium.com.cn>
2025-12-10 09:07:04 +0800

    spi: phytium: Use global-cs register to select chip by default
    The driver uses global-cs register(0x100) for chip selection by
    default. Slove the problem of not being able to read the device
    ID when using the internal chip selection register(0x10).
    
    Mainline: Open-Source
    Signed-off-by: Peng Min <pengmin1540@phytium.com.cn>
    Signed-off-by: Wang Yinfeng <wangyinfeng@phytium.com.cn>
    Change-Id: I9273042d304751510dd0acddd9ab982f874439f9

--- drivers/spi/spi-phytium-plat.c
+++ drivers/spi/spi-phytium-plat.c
@@ -25,7 +25,7 @@
 #include "spi-phytium.h"
 
 #define DRIVER_NAME "phytium_spi"
-#define DRIVER_VERSION	"1.0.0"
+#define DRIVER_VERSION	"1.0.1"
 
 #define SPI_PHYTIUM_DEFAULT_CLK_RATE	50000000
 
@@ -37,7 +37,7 @@ static int phytium_spi_probe(struct platform_device *pdev)
 	int ret;
 	int num_cs;
 	int cs_gpio;
-	int global_cs = 0;
+	int global_cs = 1;
 	int i;
 	u32 clk_rate = SPI_PHYTIUM_DEFAULT_CLK_RATE;
 
