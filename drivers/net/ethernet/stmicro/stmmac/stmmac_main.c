507986f61cad429b9d6467aae875ccf27e22a46d
zuoqian <zuoqian2032@phytium.com.cn>
2025-12-23 19:05:55 +0800

    net: stmmac: start phy instance before stmmac_hw_setup()
    Start phy and resume back the PHY to supply RX clock to MAC
    before MAC layer initialization by calling.stmmac_hw_setup(),
    since DMA reset depends on the RX clock,otherwise
    DMA reset cost maximum timeout value then finally timeout.
    
    Similar upstream commit: 36d18b5664ef
    ("net: stmmac: start phylink instance before stmmac_hw_setup()")
    
    Signed-off-by: zuoqian <zuoqian2032@phytium.com.cn>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4601,6 +4601,9 @@ int stmmac_resume(struct device *dev)
 
 	netif_device_attach(ndev);
 
+	if (ndev->phydev)
+		phy_start(ndev->phydev);
+
 	mutex_lock(&priv->lock);
 
 	stmmac_reset_queues_param(priv);
@@ -4615,9 +4618,6 @@ int stmmac_resume(struct device *dev)
 	stmmac_enable_all_queues(priv);
 
 	mutex_unlock(&priv->lock);
-
-	if (ndev->phydev)
-		phy_start(ndev->phydev);
 
 	return 0;
 }
